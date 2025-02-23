ce94e3a2e73cbd5ed626dd7dfe81fcf56646cfaa
Jujuwryy <155733830+Jujuwryy@users.noreply.github.com>
2025-02-23 12:19:28 +0300

    Create DoubleToBsonConverter.java

---
+++ main/DoubleToBsonConverter.java
@@ -0,0 +1,12 @@
+import org.bson.BsonDouble;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
+
+@WritingConverter
+public class DoubleToBsonConverter implements Converter<Double, BsonDouble> {
+
+    @Override
+    public BsonDouble convert(Double source) {
+        return new BsonDouble(source);
+    }
+}
