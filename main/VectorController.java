b35c81dc2705f457f686eeff55bcda97a6b90aa4
Jujuwryy <155733830+Jujuwryy@users.noreply.github.com>
2025-02-23 12:18:53 +0300

    Update VectorController.java

--- main/VectorController.java
+++ main/VectorController.java
@@ -1,5 +1,3 @@
-package com.george.Vector;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
