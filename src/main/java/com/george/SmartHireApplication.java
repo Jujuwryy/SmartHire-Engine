0854cb0005d315f616844709db11fc2c69de5cc4
Jujuwryy <georgemahfoud2@gmail.com>
2025-11-23 22:13:39 +0300

    Enable AOP support in Spring Boot application

--- src/main/java/com/george/SmartHireApplication.java
+++ src/main/java/com/george/SmartHireApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAspectJAutoProxy
+@EnableConfigurationProperties(AppProperties.class)
 public class SmartHireApplication {
     
     public static void main(String[] args) {
